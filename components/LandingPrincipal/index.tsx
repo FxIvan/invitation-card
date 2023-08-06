591c2de64aa4d4eaa7f8a35f8c30aa1e693c1aae
FxIvan <almendraivan210814@gmail.com>
2023-08-06 11:54:51 -0300

    V1 Diseño

--- components/LandingPrincipal/index.tsx
+++ components/LandingPrincipal/index.tsx
@@ -16,13 +16,13 @@ export default function LandingPrincipal() {
           className="mx-auto h-screen w-full md:w-1/2 object-cover absolute z-0 md:left-[25%] brightness-50 "
         />
       </div>
-      <div className="relative z-10 flex flex-col justify-start h-screen md:w-1/2 mx-auto">
+      <div className="relative z-10 flex flex-col justify-around h-screen md:w-1/2 mx-auto">
         <div className="mx-4 text-white text-sm flex flex-col justify-center">
-          <div className="text-clamp-xl-3 mt-8 mb-16 text-center">
+          <div className="text-clamp-xl-3 my-16 text-center">
             <h1>LOURDES</h1>
           </div>
           <div className="w-full ">
-            <div className="w-[80%] mx-auto">
+            <div className="w-[90%] mx-auto text-center">
               <p>
                 Hay momentos inolvidables que se atesoran en el corazon para
                 siempre por esa razón, quiero que compartas conmigo este dia tan
@@ -30,35 +30,34 @@ export default function LandingPrincipal() {
               </p>
             </div>
           </div>
-          <div className="my-8">
+        </div>
+        <div className=" text-white">
+          <div className="text-center">
+            <h2 className="text-4xl">Mis 15 años</h2>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex flex-row text-white justify-between mx-4 text-center items-center">
+            <div className="w-1/3 border-slate-50 border-r-2">Septiembre</div>
+            <div className="w-1/3 border-slate-50 border-r-2 text-4xl">02</div>
+            <div className="w-1/3">2023</div>
+          </div>
+          <div className="mx-4 text-center py-4 text-white my-8">
             <div className="text-center">
-              <h2 className="text-4xl">Mis 15 años</h2>
+              <h4>Ubicacion</h4>
+            </div>
+            <div className="text-lg">
+              📍 San Martin 515, Quilmes, Buenos Aires
+            </div>
+            <div className="text-sm my-4">
+              Salón Sede Central Empleado de comercio
             </div>
           </div>
         </div>
-        <div className="flex flex-row text-white justify-between mx-4 text-center py-4 items-center">
-          <div className="w-1/3 border-slate-50 border-r-2">Septiembre</div>
-          <div className="w-1/3 border-slate-50 border-r-2 text-4xl">02</div>
-          <div className="w-1/3">2023</div>
-        </div>
-        <div className="mx-4 text-center py-4 text-white my-8">
-          <div className="text-center my-8">
-            <h4>Ubicacion</h4>
-          </div>
-          <div className="text-lg">
-            📍 San Martin 515, Quilmes, Buenos Aires
-          </div>
-          <div className="text-sm my-4">
-            Salón Sede Central Empleado de comercio
-          </div>
-          <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-              Confirmar asistencia
-            </button>
-          </div>
-        </div>
-        <div className="absolute bottom-0 right-1/2 py-4">
-          <Image src={empleadoDeComercio} alt="SEC" width={30} height={30} />
+        <div className="my-8 text-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Confirmar asistencia
+          </button>
         </div>
       </div>
     </section>
